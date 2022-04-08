8e8b91d3d47dc2ffc2e44a3e50099771a30e30db
Clay Mullis <claymullis@fastmail.com>
2022-04-08 18:24:16 +0000

    fp16 support, sample from model every sample_interval

--- latent_diffusion_deepspeed/finetune.py
+++ latent_diffusion_deepspeed/finetune.py
@@ -5,7 +5,7 @@ import os
 import random
 from re import A
 from time import time
-
+from torchvision.transforms.functional import to_pil_image
 import torch
 from dalle_pytorch import distributed_utils
 from matplotlib import use
@@ -13,7 +13,7 @@ from matplotlib import use
 from deepspeed_config import distributed_setup
 from encoders.modules import BERTEmbedder
 from guided_diffusion.image_text_datasets import load_data
-from guided_diffusion.script_util import (create_model_and_diffusion, model_and_diffusion_defaults)
+from guided_diffusion.script_util import (create_model_and_diffusion, model_and_diffusion_defaults, create_gaussian_diffusion)
 
 def set_requires_grad(model, value):
     for param in model.parameters():
@@ -24,7 +24,6 @@ def load_encoder(model, requires_grad=False):
     encoder = torch.load(model, map_location="cpu")
     encoder.eval()
     set_requires_grad(encoder, requires_grad)
-    del encoder.decoder
     del encoder.loss
     return encoder
 
@@ -38,8 +37,7 @@ def load_bert(device, bert_path='bert.ckpt', requires_grad=False):
     set_requires_grad(bert, requires_grad)
     return bert
 
-
-def load_model_and_diffusion(model_path, use_fp16=True):
+def diffusion_options(use_fp16, timestep_respacing):
     options = model_and_diffusion_defaults()
     options.update({
         "attention_resolutions": "32,16,8",
@@ -52,9 +50,15 @@ def load_model_and_diffusion(model_path, use_fp16=True):
         "num_heads": 8,
         "num_res_blocks": 2,
         "resblock_updown": False,
+        "timestep_respacing": timestep_respacing,
         "use_fp16": use_fp16,
         "use_scale_shift_norm": False,
     })
+    return options
+
+
+def load_model_and_diffusion(model_path, use_fp16=True):
+    options = diffusion_options(use_fp16, timestep_respacing=str(1000))
     model, diffusion = create_model_and_diffusion(**options)
     if len(model_path) > 0:
         assert os.path.exists(model_path), "model path does not exist"
@@ -74,30 +78,31 @@ def load_latent_data(encoder, bert, data_dir, batch_size, image_size, device, ra
         random_flip=random_flip,
         use_webdataset=use_webdataset,
     )
-    for batch, model_kwargs, text in dataloader:
-        batch = batch.to(device)
-        text_emb = bert.encode(list(text)).to(device)
-        text_blank = bert.encode(['']*batch.shape[0]).to(device)
+    with torch.cuda.amp.autocast(enabled=use_fp16):
+        for batch, model_kwargs, text in dataloader:
+            batch = batch.to(device)
+            text_emb = bert.encode(list(text)).to(device)
+            text_blank = bert.encode(['']*batch.shape[0]).to(device)
 
-        for i in range(batch.shape[0]):
-            if random.randint(0,100) < 20:
-                text_emb[i] = text_blank[i]
+            for i in range(batch.shape[0]):
+                if random.randint(0,100) < 20:
+                    text_emb[i] = text_blank[i]
 
-        model_kwargs["context"] = text_emb
+            model_kwargs["context"] = text_emb
+            batch = batch.to(device)
+            emb = encoder.encode(batch).sample()
+            emb *= 0.18215
+            yield emb, model_kwargs
 
-        batch = batch.to(device)
-        emb = encoder.encode(batch).sample()
-        emb *= 0.18215
-
-        yield emb, model_kwargs
 
 def train_step(model, diffusion, x_start, device, model_kwargs={}):
-    model_kwargs["context"].to(device)
-    timesteps = torch.randint(0, len(diffusion.betas) - 1, (x_start.shape[0],), device=device)
-    scaled_timesteps = diffusion._scale_timesteps(timesteps).to(device)
+    with torch.no_grad():
+        model_kwargs["context"].to(device)
+        timesteps = torch.randint(0, len(diffusion.betas) - 1, (x_start.shape[0],), device=device)
+        scaled_timesteps = diffusion._scale_timesteps(timesteps).to(device)
 
-    noise = torch.randn_like(x_start, device=device)
-    x_t = diffusion.q_sample(x_start, timesteps, noise=noise).to(device)
+        noise = torch.randn_like(x_start, device=device)
+        x_t = diffusion.q_sample(x_start, timesteps, noise=noise).to(device)
 
     epsilon = model(x_t, scaled_timesteps, **model_kwargs).to(device)
     return torch.nn.functional.mse_loss(epsilon, noise.detach())
@@ -107,6 +112,60 @@ def save_model(model, model_path):
     os.makedirs(os.path.dirname(model_path), exist_ok=True)
     torch.save(model.state_dict(), model_path)
 
+@torch.inference_mode()
+def sample(idx, text, bert, ldm, model, batch_size, device, prefix="output", timestep_respacing="50", ddpm=False, guidance_scale=10.0, shape=(256, 256)):
+    sampling_options = diffusion_options(False, timestep_respacing)
+    sampling_diffusion = create_gaussian_diffusion(
+        steps=sampling_options["diffusion_steps"],
+        learn_sigma=sampling_options["learn_sigma"],
+        noise_schedule=sampling_options["noise_schedule"],
+        use_kl=sampling_options["use_kl"],
+        predict_xstart=sampling_options["predict_xstart"],
+        rescale_timesteps=sampling_options["rescale_timesteps"],
+        rescale_learned_sigmas=sampling_options["rescale_learned_sigmas"],
+        timestep_respacing=timestep_respacing,
+    )
+    height, width = shape
+    text_emb = bert.encode([text]*batch_size).to(device)
+    text_blank = bert.encode(['']*batch_size).to(device)
+    kwargs = { "context": torch.cat([text_emb, text_blank], dim=0) }
+    # Create a classifier-free guidance sampling function
+    def model_fn(x_t, ts, **kwargs):
+        half = x_t[: len(x_t) // 2]
+        combined = torch.cat([half, half], dim=0)
+        model_out = model(combined, ts, **kwargs)
+        eps, rest = model_out[:, :3], model_out[:, 3:]
+        cond_eps, uncond_eps = torch.split(eps, len(eps) // 2, dim=0)
+        half_eps = uncond_eps + guidance_scale * (cond_eps - uncond_eps)
+        eps = torch.cat([half_eps, half_eps], dim=0)
+        return torch.cat([eps, rest], dim=1)
+
+    if timestep_respacing.startswith('ddim'):
+        sample_fn = sampling_diffusion.ddim_sample_loop_progressive
+    elif ddpm:
+        sample_fn = sampling_diffusion.p_sample_loop_progressive
+    else:
+        sample_fn = sampling_diffusion.plms_sample_loop_progressive
+
+    samples = sample_fn(
+        model_fn,
+        (batch_size*2, 4, int(height/8), int(width/8)),
+        clip_denoised=False,
+        model_kwargs=kwargs,
+        cond_fn=None,
+        device=device,
+        progress=False,
+    )
+    for sample in samples:
+        for k, image in enumerate(sample['pred_xstart'][:batch_size]):
+            image /= 0.18215
+            im = image.unsqueeze(0)
+            out = ldm.decode(im)
+            out = to_pil_image(out.squeeze(0).add(1).div(2).clamp(0, 1))
+            os.makedirs(os.path.dirname(f"{prefix}/{idx}/{k}.png"), exist_ok=True)
+            out.save(f"{prefix}/{idx}/{k}.png")
+
+    out.save("current_sample.png")
 
 def main():
     parser = argparse.ArgumentParser()
@@ -125,6 +184,7 @@ def main():
     parser.add_argument("--weight_decay", type=float, default=0.0)
     parser.add_argument("--seed", type=int, default=0)
     parser.add_argument("--log_interval", type=int, default=10)
+    parser.add_argument("--sample_interval", type=int, default=100)
     parser.add_argument("--save_interval", type=int, default=1000)
     parser.add_argument("--num_workers", type=int, default=0)
     parser.add_argument("--max_steps", type=int, default=1000000)
@@ -159,6 +219,7 @@ def main():
 
     bert.to(device)
     encoder.to(device)
+
     # Load data
     data = load_latent_data(encoder, bert, data_dir, args.batch_size, args.image_size, device, random_flip=args.random_flip, random_crop=args.random_crop, use_webdataset=args.use_webdataset, distr_backend=distr_backend, use_fp16=args.use_fp16)
 
@@ -191,6 +252,8 @@ def main():
                 accumulated_loss = loss
             if i % args.log_interval == 0 and is_root_rank:
                 print(f"epoch {epoch} step {i} loss {accumulated_loss.item()}")
+            if i % args.sample_interval == 0 and is_root_rank:
+                sample(idx=i, text="an avocado in the form of an armchair", bert=bert, ldm=encoder, model=model, batch_size=1, device=device, timestep_respacing="40", ddpm=False, guidance_scale=4.0, shape=(256, 256))
             if i % args.save_interval == 0 and is_root_rank:
                 save_model(model, os.path.join(
                     args.log_dir, f"diffusion-{epoch}-{i}.pt"))
