2aad686e2290c621610be41c5db3b4760965500e
Clay Mullis <claymullis@fastmail.com>
2022-04-08 18:22:35 +0000

    update example train script

--- train.sh
+++ train.sh
@@ -6,21 +6,23 @@ deepspeed --include localhost:0,1,2,3,4,5,6,7 latent_diffusion_deepspeed/finetun
     --data_dir $data_dir \
     --log_dir $log_dir \
     --image_size 256 \
-    --batch_size 64 \
-    --ga_steps 1 \
+    --batch_size 112 \
+    --ga_steps 8 \
     --kl_model 'kl-f8.pt' \
     --bert_path 'bert.ckpt' \
     --resume_ckpt 'diffusion.pt' \
-    --num_epochs 100 \
-    --lr 2e-5 \
-    --min_lr 1e-6 \
+    --num_epochs 10 \
+    --lr 1e-4 \
+    --min_lr 7e-6 \
     --weight_decay 0.01 \
     --seed 0 \
-    --log_interval 10 \
-    --save_interval 1000 \
-    --num_workers 0 \
-    --max_steps 100000 \
     --random_flip \
-    --random_crop \
+    --log_interval 5 \
+    --save_interval 1000 \
+    --sample_interval 100 \
+    --num_workers 8 \
+    --max_steps 50000 \
+    --warmup_steps 1000 \
     --use_webdataset \
-    --deepspeed
+    --deepspeed \
+    --use_fp16
