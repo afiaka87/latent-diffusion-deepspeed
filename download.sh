ec4b8b60c3c06b50d0466d2b7a2a0f7ddfeada63
Clay Mullis <claymullis@fastmail.com>
2022-04-10 18:30:08 +0000

    add encoders code from Jack0

---
+++ download.sh
@@ -0,0 +1,4 @@
+# Download checkpoints from Jack0's mirror. 
+wget --continue https://dall-3.com/models/glid-3-xl/bert.pt -o bert.ckpt
+wget --continue https://dall-3.com/models/glid-3-xl/kl-f8.pt
+wget --continue https://dall-3.com/models/glid-3-xl/diffusion.pt
