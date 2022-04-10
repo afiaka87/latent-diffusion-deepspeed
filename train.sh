1cb89646cc2d69bd153ae014e5cd5a44f6804f99
Clay Mullis <claymullis@fastmail.com>
2022-04-10 18:11:55 +0000

    Fix training sh script

--- train.sh
+++ train.sh
@@ -1,8 +1,8 @@
 export TOKENIZERS_PARALLELISM=false # required to avoid errors with transformers lib
-data_dir="data/coco/my_data"
-# data_dir="s3://my-s3/path/to/webdataset/{00000..00161}.tar"
+# data_dir="data/coco/my_data"
+data_dir="s3://my-s3/path/to/webdataset/{00000..00161}.tar"
 log_dir="my-logs"
-deepspeed --include localhost:0,1,2,3,4,5,6,7 latent_diffusion_deepspeed/finetune.py \
+deepspeed --include localhost:0,1,2,3,4,5,6,7 finetune.py \
     --data_dir $data_dir \
     --log_dir $log_dir \
     --image_size 256 \
@@ -16,9 +16,9 @@ deepspeed --include localhost:0,1,2,3,4,5,6,7 latent_diffusion_deepspeed/finetun
     --min_lr 1e-6 \
     --weight_decay 0.01 \
     --seed 0 \
-    --log_interval 5 \
+    --log_interval 10 \
     --save_interval 1000 \
-    --sample_interval 50 \
+    --sample_interval 100 \
     --num_workers 8 \
     --max_steps 1_000_000 \
     --warmup_steps 1000 \
